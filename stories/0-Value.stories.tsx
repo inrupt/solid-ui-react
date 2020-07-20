ea08928df0e76579c0a83d81cd92b0f94affc117
Jack Lawson <ajacksified@gmail.com>
2020-07-20 02:37:37 -0400

    Initial storybook setup

---
+++ stories/0-Value.stories.tsx
@@ -0,0 +1,12 @@
+import React, { ReactElement } from 'react';
+
+import Value from "../src/value";
+
+export default {
+  title: "Value",
+  component: Value,
+};
+
+export function valueExample(): ReactElement {
+  return <Value />;
+}
