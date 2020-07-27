cb7ca38dd5cd9daf97af917e82a5ad07ee857f50
Andy Davison <adavison@and.digital>
2020-07-27 17:12:26 +0100

    Add Link stories

---
+++ stories/Link.stories.tsx
@@ -0,0 +1,55 @@
+/**
+ * Copyright 2020 Inrupt Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+ * Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+import React, { ReactElement } from "react";
+import * as LitPodFns from "@solid/lit-pod";
+import Link from "../src/Link";
+
+export default {
+  title: "Link",
+  component: Link,
+};
+
+export function WithoutChildren(): ReactElement {
+  const exampleUrl = "http://test.url";
+  const examplePredicate = `https://www.w3.org/ns/auth/acl#origin`;
+  const exampleThing = LitPodFns.addUrl(
+    LitPodFns.createThing(),
+    examplePredicate,
+    exampleUrl
+  );
+  return <Link thing={exampleThing} property={examplePredicate} />;
+}
+
+export function WithChildren(): ReactElement {
+  const exampleUrl = "http://test.url";
+  const examplePredicate = `https://www.w3.org/ns/auth/acl#origin`;
+  const exampleThing = LitPodFns.addUrl(
+    LitPodFns.createThing(),
+    examplePredicate,
+    exampleUrl
+  );
+  return (
+    <Link thing={exampleThing} property={examplePredicate}>
+      <span>Example child</span>
+    </Link>
+  );
+}
