d8a26f09487625520c0dbda9be35aa2e8317796d
Andy Davison <adavison@and.digital>
2020-08-25 12:16:06 +0100

    Add initial Table storybook

---
+++ stories/table.stories.tsx
@@ -0,0 +1,84 @@
+/**
+ * Copyright 2020 Inrupt Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+ * Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+import React, { ReactElement } from "react";
+import * as SolidFns from "@inrupt/solid-client";
+import { withKnobs, text, boolean, object } from "@storybook/addon-knobs";
+import { action } from "@storybook/addon-actions";
+import {
+  Table,
+  TableColumn,
+  TableColumnHeading,
+} from "../src/components/table";
+
+export default {
+  title: "Components/Table",
+  component: Table,
+  decorators: [withKnobs],
+};
+
+const inputOptions = {
+  type: "text",
+  name: "test-name",
+  className: "test-class",
+};
+
+export function BasicExample(): ReactElement {
+  const namePredicate = `http://xmlns.com/foaf/0.1/name`;
+  const nickPredicate = `http://xmlns.com/foaf/0.1/nick`;
+
+  const thing1A = SolidFns.addStringNoLocale(
+    SolidFns.createThing(),
+    namePredicate,
+    `example name 1`
+  );
+
+  const thing1 = SolidFns.addStringNoLocale(
+    thing1A,
+    nickPredicate,
+    `example nick 1`
+  );
+
+  const thing2A = SolidFns.addStringNoLocale(
+    SolidFns.createThing(),
+    namePredicate,
+    `example name 2`
+  );
+
+  const thing2 = SolidFns.addStringNoLocale(
+    thing2A,
+    nickPredicate,
+    `example nick 2`
+  );
+
+  return (
+    <Table things={[thing1, thing2]}>
+      <TableColumn>
+        <TableColumnHeading property={namePredicate}>
+          <span>Test heading testing testing</span>
+        </TableColumnHeading>
+      </TableColumn>
+      <TableColumn>
+        <TableColumnHeading property={nickPredicate} />
+      </TableColumn>
+    </Table>
+  );
+}
