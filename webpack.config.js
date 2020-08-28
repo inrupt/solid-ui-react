f3e589e9b7175bb8336b73ef7f287a647c7ed930
Jack Lawson <ajacksified@gmail.com>
2020-08-28 10:45:40 -0400

    Enable sourcemaps

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
+  devtool: "source-map",
   entry: "./src/index.ts",
   module: {
     rules: [
