919eabb82a8717bc9558f6410260206b776083e6
Andy Davison <adavison@and.digital>
2020-10-27 14:19:07 +0000

    Return login/logout from useSession

--- src/hooks/useSession/index.tsx
+++ src/hooks/useSession/index.tsx
@@ -20,23 +20,34 @@
  */
 
 import { useContext } from "react";
-import { Session } from "@inrupt/solid-client-authn-browser";
+import {
+  ILoginInputOptions,
+  Session,
+} from "@inrupt/solid-client-authn-browser";
 import { SessionContext } from "../../context/sessionContext";
 
 export interface SessionInfo {
   session: Session;
   sessionRequestInProgress: boolean;
   fetch: typeof window.fetch;
+  login: (options: ILoginInputOptions) => Promise<void>;
+  logout: () => Promise<void>;
 }
 
 export default function useSession(): SessionInfo {
-  const { session, sessionRequestInProgress, fetch } = useContext(
-    SessionContext
-  );
+  const {
+    session,
+    sessionRequestInProgress,
+    fetch,
+    login,
+    logout,
+  } = useContext(SessionContext);
 
   return {
     session,
     sessionRequestInProgress,
     fetch,
+    login,
+    logout,
   };
 }
