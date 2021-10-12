eb0ddca295a3f7fc0c24f0bc320008c5ebf08a16
Nicolas Seydoux <nseydoux@inrupt.com>
2021-10-12 12:40:07 +0200

    Add custom jest env
    This supports solid-client-authn-browser

---
+++ tests/environment/customEnvironment.js
@@ -0,0 +1,15 @@
+const Environment = require("jest-environment-jsdom");
+
+// Custom test environment copied from https://github.com/jsdom/jsdom/issues/2524
+// in order to add TextEncoder to jsdom. TextEncoder is expected by jose.
+
+module.exports = class CustomTestEnvironment extends Environment {
+  async setup() {
+    await super.setup();
+    if (typeof this.global.TextEncoder === "undefined") {
+      const { TextEncoder, TextDecoder } = require("util");
+      this.global.TextEncoder = TextEncoder;
+      this.global.TextDecoder = TextDecoder;
+    }
+  }
+};
