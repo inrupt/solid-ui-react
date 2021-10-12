ccb6ba803e648c55e2eb18904a8e3f18021f4e11
Nicolas Seydoux <nseydoux@inrupt.com>
2021-10-12 14:31:04 +0200

    Temp. stop depending on shared linting rules
    The shared linting rules are out-of-date, and need to be cleaned up and republished before being depended on again.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,27 @@
+/* eslint-disable license-header/header */
+
 module.exports = {
   root: true,
-  plugins: ["license-header"],
-  extends: ["@inrupt/eslint-config-react"],
+  plugins: ["license-header", "@typescript-eslint", "jest", "react", "jsx-a11y"],
+  // TODO: 
+  // extends: ["@inrupt/eslint-config-react"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:jest/recommended",
+    "plugin:jest/style",
+  ],
+  parser: "@typescript-eslint/parser",
   rules: {
     "@typescript-eslint/ban-ts-comment": 0,
     "license-header/header": [1, "./resources/license-header.js"],
     "no-use-before-define": [0],
     "@typescript-eslint/no-use-before-define": [1],
+    "no-unused-vars": [0]
 
-    "prettier/prettier": [
-      "error",
-      { endOfLine: "auto" },
-    ],
+    // "prettier/prettier": [
+    //   "error",
+    //   { endOfLine: "auto" },
+    // ],
   },
 }
