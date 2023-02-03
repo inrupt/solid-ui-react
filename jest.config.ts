4de9c840d78b90c715b4c34b4166007926e4fd91
Virginia Balseiro <virginia.balseiro@inrupt.com>
2023-02-03 10:59:28 +0100

    Chore: update jest to v29 and use JSDom polyfill (#739)
    * Chore: update jest to v29 and use JSDom polyfill
    
    - update jest
    - use JSDom polyfill
    - migrate jest config file to TS
    
    * Update jest.config.ts
    
    Co-authored-by: Zwifi <nseydoux@inrupt.com>
    
    ---------
    
    Co-authored-by: Zwifi <nseydoux@inrupt.com>

---
+++ jest.config.ts
@@ -0,0 +1,50 @@
+/**
+ * Copyright 2020 Inrupt Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+ * Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+export default {
+  // Automatically clear mock calls and instances between every test
+  clearMocks: true,
+
+  // The test environment that will be used for testing
+  testEnvironment: "jsdom",
+
+  testPathIgnorePatterns: ["/node_modules/", "/__testUtils/"],
+
+  // This combination of preset/transformIgnorePatterns enforces that both TS and
+  // JS files are transformed to CJS, and that the transform also applies to the
+  // dependencies in the node_modules, so that ESM-only dependencies are supported.
+  preset: "ts-jest/presets/js-with-ts",
+  // deliberately set to an empty array to allow including node_modules when transforming code:
+  transformIgnorePatterns: [],
+  collectCoverage: true,
+  coverageReporters: process.env.CI ? ["text", "lcov"] : ["text"],
+
+  coveragePathIgnorePatterns: ["/node_modules/", "<rootDir>/dist"],
+
+  coverageThreshold: {
+    global: {
+      branches: 88,
+      functions: 90,
+      lines: 94,
+      statements: 94,
+    },
+  },
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+};
