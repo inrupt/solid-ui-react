5233b9d1299f87868c1de89eee66ff64307d8ff0
Jesse Wright <63333554+jeswr@users.noreply.github.com>
2023-08-08 15:53:57 +1000

    chore: migrate to shared rollup config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -19,7 +19,7 @@
 // SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 import { createRequire } from "node:module";
-import createConfig from "./rollup.common.mjs";
+import createConfig from "@inrupt/base-rollup-config";
 const require = createRequire(import.meta.url);
 
 export default createConfig(require("./package.json"));
